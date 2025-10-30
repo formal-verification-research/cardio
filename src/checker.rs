f803dd84698028cbfd4497ba81373cf504e1e4fb
Josh Jeppson <joshua.jeppson@usu.edu>
2025-10-30 14:13:55 -0600

    some fun stuff

---
+++ src/checker.rs
@@ -0,0 +1,17 @@
+use crate::matrix::*;
+use crate::*;
+
+/// A CTMC transition matrix
+pub trait CtmcTransMat {
+	fn uniformize(rate_mat: Self) -> Self;
+}
+
+impl<EntryType> CtmcTransMat for sprs::CsMat<EntryType>
+where
+	EntryType: num::Num + Clone,
+{
+	fn uniformize(rate_mat: Self) -> Self {
+	    unimplemented!();
+	}
+}
+
