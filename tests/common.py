352039abb1d0fae1110c9dd5f1d15f78c1d9b437
Josh Jeppson <joshua.jeppson@usu.edu>
2026-01-26 14:01:16 -0700

    working on finding bugs

--- tests/common.py
+++ tests/common.py
@@ -1,7 +1,7 @@
-from numpy import *
 import cardio
 import stormpy
 
+import numpy as np
 from collections import deque
 
 class Transition:
@@ -9,7 +9,7 @@ class Transition:
 		self.update = update
 		self.needed = needed
 		if needed is None:
-			self.needed = np.zeros(self.update.size())
+			self.needed = np.zeros(self.update.size)
 		self.rate_fn = rate_fn
 
 	def get_update(self, cur_state: np.matrix):
@@ -26,21 +26,24 @@ class Model:
 		self.sat_predicate = sat_predicate
 
 	def next_states(self, state):
-		updates = [transition.update(state) for transition in self.transitions]
+		updates = [transition.get_update(state) for transition in self.transitions]
 		return [update for update in updates if update is not None and (update[0] > self.var_bound).all()]
 
 	def check_cardio_and_storm(self, time_bound):
 		next_available_index = 2 # absorbing is 0 init is 1
 		queue = deque([self.init_state])
-		state_to_id = {tuple(cur_state):1}
+		init_tuple = tuple(self.init_state.T.tolist()[0])
+		state_to_id = {init_tuple:1}
 		# Create matrices for cardio and storm
 		cardio_rf = cardio.QuantitativeReachabilityFinder()
 		stormpy_mat = stormpy.SparseMatrixBuilder()
-		stormpy_labels = stormpy.StateLabeling()
-		stormpy_labels.add_label_to_state("absorbing", 0)
+
+		sat_indecies = []
+
 		while len(queue) > 0:
 			cur_state = queue.popleft()
-			cur_idx = state_to_id[tuple(cur_state)]
+			cur_state_tuple = tuple(cur_state.T.tolist()[0])
+			cur_idx = state_to_id[tuple(cur_state_tuple)]
 			updates = self.next_states(cur_state)
 			for next_state, rate in updates:
 				next_idx = -1
@@ -53,7 +56,7 @@ class Model:
 					# Check if satisfying
 					if self.sat_predicate(next_state):
 						cardio_rf.set_state_satisfying(next_idx)
-						stormpy_labels.add_label_to_state("satisfying", next_idx)
+						sat_indecies.append(next_idx)
 				# Add to both matrices
 				rf.insert(cur_idx, next_idx, rate)
 				mat.add_next_value(cur_idx, next_idx, rate)
@@ -64,6 +67,11 @@ class Model:
 		lower_bound, upper_bound = cardio_rf.build_matrix_and_get_bounds(time_bound)
 		print(f"Cardio returned bound {lower_bound}, {upper_bound}")
 		print("Checking model with storm")
+		# We have to build labeling for storm
+		stormpy_labels = stormpy.StateLabeling()
+		stormpy_labels.add_label_to_state("absorbing", 0)
+		for idx in sat_indecies:
+			stormpy_labels.add_label_to_state("satisfying", next_idx)
 		storm_ctmc = stormpy.SparseModelCtmc(transition_matrix=stormpy_mat.build(), state_labeling=stormpy_labels, rate_transitions=True)
 		props_strs = [ f"P=? [ true U{time_bound} \"satisfying\" ]", f"P=? [ true U{time_bound} \"satisfying\" | \"absorbing\" ]" ]
 		lprop, rprop = stormpy.parse_properties(props_strs)
--- tests/toy.py
+++ tests/toy.py
@@ -1,5 +1,21 @@
-from .common import *
+#!/usr/bin/env python3
+
+from common import *
+
+import numpy as np
 
 transitions = [
-	Transition(
+	Transition(np.matrix([1.0, -2.0, 0, 0]).T, None, lambda state: 2.0 * state[0]),
+	Transition(np.matrix([-1.0, 2.0, 0, 0]).T, None, lambda state: 1.3 * state[1]),
+	Transition(np.matrix([-1.0, 2.0, 1.0, 0]).T, None, lambda state: 0.3 * state[2]),
+	Transition(np.matrix([1.0, -2.0, 0, 1.0]).T, None, lambda state: 0.3 * state[2]),
+	Transition(np.matrix([0, 0, 0, -1.0]).T, None, lambda state: 1.3 * state[3])
 ]
+
+init_state = np.matrix([0,0,0,0], dtype="float64").T
+
+sat_predicate = lambda state: state[3] >= 50
+
+model = Model(init_state, transitions, 100, sat_predicate)
+
+model.check_cardio_and_storm(100.0)
